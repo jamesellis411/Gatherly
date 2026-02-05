55c078b2f6a17f401476d96fca42930aff7f3527
jamesellisunc <jamellis@ad.unc.edu>
2026-02-05 11:33:46 -0500

    Initial Commit

---
+++ Gatherly/GatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatherlyApp.swift
+//  Gatherly
+//
+//  Created by James Ellis on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct GatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
